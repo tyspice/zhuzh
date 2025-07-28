741b448aa28125d55bbd292338ebb5b6ea376488
tyspice <tyner@tyspice.dev>
2025-07-27 18:10:32 -0700

    add config

--- go.mod
+++ go.mod
@@ -37,4 +37,5 @@ require (
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/term v0.31.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/config/config.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"sync"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ChatGPT struct {
+		ApiKey string `yaml:"api_key"`
+		Model  string `yaml:"model"`
+	} `yaml:"chatgpt"`
+}
+
+type ConfigOption struct {
+	ChatGPTApiKey *string
+	ChatGPTModel  *string
+}
+
+var (
+	instance *Config
+	once     sync.Once
+)
+
+// Init creates the singleton instance by first reading from ~/.config/zhuzh/config.yml
+// and then overriding with any provided options
+// This should be called once at application startup
+func Init(opts *ConfigOption) *Config {
+	once.Do(func() {
+		instance = &Config{}
+
+		// Read from YAML file first
+		homeDir, err := os.UserHomeDir()
+		if err == nil {
+			configPath := filepath.Join(homeDir, ".config", "zhuzh", "config.yml")
+			if data, err := os.ReadFile(configPath); err == nil {
+				// Ignore YAML parsing errors and continue with defaults
+				yaml.Unmarshal(data, instance)
+			}
+		}
+
+		// Override with provided options
+		if opts != nil {
+			if opts.ChatGPTApiKey != nil {
+				instance.ChatGPT.ApiKey = *opts.ChatGPTApiKey
+			}
+			if opts.ChatGPTModel != nil {
+				instance.ChatGPT.Model = *opts.ChatGPTModel
+			}
+		}
+
+	})
+	return instance
+}
+
+// Get retrieves the singleton instance. Must call Init first
+// or this function will panic
+func Get() *Config {
+	if instance == nil {
+		panic("config not initialized - call Init() before accessing the config")
+	}
+	return instance
+}
