395fee55b853e682c6fe83248a8a1a14fab4f8dd
tyspice <tyner@tyspice.dev>
2025-08-03 17:03:26 -0700

    remove commented code

--- main.go
+++ main.go
@@ -3,25 +3,5 @@ package main
 import "github.com/tyspice/zhuzh/internal/ui"
 
 func main() {
-
 	ui.Run()
-
-	// prompt := os.Args[1]
-	// responseChan, errorChan := chatgpt.StreamResponse(prompt)
-
-	// for {
-	// 	select {
-	// 	case resp, ok := <-responseChan:
-	// 		if !ok {
-	// 			fmt.Print("\n\nAll done!\n")
-	// 			return // Channel closed, streaming finished
-	// 		}
-	// 		fmt.Print(resp)
-	// 	case err := <-errorChan:
-	// 		if err != nil {
-	// 			fmt.Printf("Error: %v\n", err)
-	// 			return
-	// 		}
-	// 	}
-	// }
 }
